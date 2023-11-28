3817d67282a3bc0ca644cd5997d0c6b28a0dca5d
Nikhil-chhajer <nikhil.chhajer80@gmail.com>
2023-11-28 18:22:12 +0530

    First commit for booking service with basic setup

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const app=express();
+
+const bodyParser=require('body-parser');
+const {PORT}=require('./config/serverConfig');
+const apiRoutes=require('./routes/index');
+const db=require('./models/index');
+const setupandstartserver=()=>{ 
+    app.use(bodyParser.json());
+    //bodyparser is use to read request body properly
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use('/api',apiRoutes);
+    app.listen(PORT,()=>{
+        console.log(`Server Started on port ${PORT}`);
+        if(process.env.DB_SYNC){
+            db.Sequelize.sync({alter:true});
+        }
+    })
+}
+setupandstartserver();
