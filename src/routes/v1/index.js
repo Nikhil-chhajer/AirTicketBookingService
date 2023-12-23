9ae068f8c0f6b1e20a7ebaf805733c13f70921b5
Nikhil-chhajer <nikhil.chhajer80@gmail.com>
2023-12-23 20:32:12 +0530

    Added message and setup the rabbitmq

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,9 @@
 const express=require('express');
+//const {createChannel}=require('../../utils/messageQueue');
 const {BookingController}=require('../../controllers/index');
+//const channel=await createChannel();
+const bookingController=new BookingController()
 const router=express.Router();
-router.post('/bookings',BookingController.create);
+router.post('/bookings',bookingController.create);
+router.post('/publish',bookingController.sendMessageToQueue);
 module.exports=router;
